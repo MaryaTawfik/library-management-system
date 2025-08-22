e42acf7924becd117dad602810ed51fbe66ea5f5
lu00009 <lelomohammedpg@gmail.com>
2025-08-22 22:56:19 +0300

    some improvement

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -3,12 +3,17 @@ const authService = require('../services/auth_service');
 const register = async (req, res) => {
   try {
     const { userID, firstName, lastName, email, password, phoneNumber } = req.body;
+    const data = { userID, firstName, lastName, email, password, phoneNumber };
 
-    if (!userID || !firstName || !lastName || !email || !password || !phoneNumber) {
+    if (!data.userID || !data.firstName || !data.lastName || !data.email || !data.password || !data.phoneNumber) {
       return res.status(400).json({ message: 'Missing required user data' });
     }
 
-    const user = await authService.registeruser(req.body);
+    const user = await authService.registeruser({
+      ...data,
+      is_member: true,
+      expiryDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) // 30 days from now
+    });
     res.status(201).json({ message: 'User registered successfully', user });
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const bookController=require('../controllers/bookController')
+const bookController = require('../controllers/bookController')
+const Role = require('../middlewares/role');
+const authMiddleware = require('../middlewares/auth');
 
-router.get('/' , bookController.getAll);
-router.get('/:id' , bookController.getOne);
-router.post('/' , bookController.create);
-router.patch('/:id' , bookController.update);
-router.delete('/:id' , bookController.remove);
+router.get('/', bookController.getAll);
+router.get('/:id', bookController.getOne);
+router.post('/', authMiddleware, Role.isAdmin, bookController.create);
+router.patch('/:id', authMiddleware, Role.isAdmin, bookController.update);
+router.delete('/:id', authMiddleware, Role.isAdmin, bookController.remove);
 
-module.exports=router;
+module.exports = router;
